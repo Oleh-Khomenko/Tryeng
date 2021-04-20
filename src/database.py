f56a83b82327004c5d34dc41b842ba8a36983454
Oleh-Khomenko <olehhomenko12@gmail.com>
2021-04-21 00:58:39 +0300

    update

--- src/database.py
+++ src/database.py
@@ -1,2 +1,17 @@
-import os
-SQLALCHEMY_DB_URL = os.getenv("DB_CONN")
+import pymysql
+
+conn = pymysql.connect(
+    host='eyw6324oty5fsovx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user='sukn3ydi0q6517dx',
+    password='gegyoqxnv18e28k3',
+    port=3306,
+    database='nunae2j7ki7ne8si'
+)
+
+with conn:
+    cur = conn.cursor()
+    cur.execute("SELECT VERSION()")
+
+    version = cur.fetchone()
+
+    print("Database version: {}".format(version[0]))
