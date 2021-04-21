936c76c7ea99cb19517b9653b5a6850e389ceb03
Oleh-Khomenko <olehhomenko12@gmail.com>
2021-04-21 10:43:56 +0300

    update

--- src/database.py
+++ src/database.py
@@ -1,17 +1,18 @@
 import pymysql
+from DB_CONN import db_conn
 
 conn = pymysql.connect(
-    host='eyw6324oty5fsovx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user='sukn3ydi0q6517dx',
-    password='gegyoqxnv18e28k3',
-    port=3306,
-    database='nunae2j7ki7ne8si'
+    host=db_conn['host'],
+    user=db_conn['user'],
+    password=db_conn['password'],
+    port=db_conn['port'],
+    database=db_conn['database']
 )
 
 with conn:
-    cur = conn.cursor()
-    cur.execute("SELECT VERSION()")
+    with conn.cursor() as cur:
+        cur.execute("SELECT VERSION()")
 
-    version = cur.fetchone()
+        version = cur.fetchone()
 
-    print("Database version: {}".format(version[0]))
+        print("Database version: {}".format(version[0]))
